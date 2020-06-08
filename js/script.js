ef5f380d78a58e67babea11ce48c8e027c68c504
BiancaFSilva <biancaflorianodasilva@gmail.com>
2020-06-08 15:08:12 -0300

    Tabuleiro script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,38 @@ var canvas = document.getElementById("tela");
 var ctx = canvas.getContext("2d");
 var btPausa = document.getElementById("btPausa");
 
-ctx.fillStyle = "#ff0000";
-ctx.fillRect (20,30,50,100);
+// Tabuleiro
+var nx = 0;
+var ny = 0;
+var largura = 20;
+var distancia = 5;
+var bordaX, bordaY;
+
+// INÍCIO
+criarTabuleiro();
+novoJogo();
+
+function criarTabuleiro() {    
+    nx = Math.floor((canvas.width - distancia) / (largura + distancia));
+    ny = Math.floor((carvas.height - distancia) / (largura + distancia));
+    bordaX = nx * (distancia + largura) + distancia;
+    bordaY = ny * (distancia + largura) + distancia;
+}
+
+function novoJogo() {
+    btPausa.innerHTML = "Iniciar";
+    btPausa.disabled = false;
+    desenhar();
+}
+
+function desenhar() {
+    var xi, yi;
+    ctx.clearRect (0, 0, canvas.width, canvas.height); // Limpa a tela
+    
+    // Desenhando as bordas
+    ctx.fillStyle = "#888888";
+    ctx.fillRect (bordaX, 0, canvas.width - 1, canvas.height - 1); 
+    ctx.fillRect (0, bordaY, canvas.width - 1, canvas.height - 1); 
+
+    // Desenhando a cobra
+}
