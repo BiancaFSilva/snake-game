63d0a02ea8517d35576e9bf8f73696d9f933b731
BiancaFSilva <biancaflorianodasilva@gmail.com>
2020-06-08 14:40:50 -0300

    Teste script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("tela");
+var ctx = canvas.getContext("2d");
+var btPausa = document.getElementById("btPausa");
+
+ctx.fillStyle = "#ff0000";
+ctx.fillRect (20,30,50,100);
